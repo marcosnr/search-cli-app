6613564550428a4e9257428f4dee0ec45b137f69
Nino-Ruiz, Marcos <mninoruiz@gmail.com>
2020-03-26 14:58:28 +1100

    [ZC-003] More Test resources
    Signed-off-by: Nino-Ruiz, Marcos <mninoruiz@gmail.com>

--- tests/app/test_data_loader.py
+++ tests/app/test_data_loader.py
@@ -2,8 +2,17 @@ import os
 import pytest
 from data_loader import DataLoader
 
-def test_load_file():
+def test_load_org_file():
   current_dir = os.path.dirname(os.path.realpath(__file__))
-  test_file = os.path.join(current_dir, 'resources', 'orgs.json')
+  test_file = os.path.join(current_dir, 'resources', 'test_orgs.json')
   assert DataLoader._load_file(test_file)
 
+def test_load_user_file():
+  current_dir = os.path.dirname(os.path.realpath(__file__))
+  test_file = os.path.join(current_dir, 'resources', 'test_users.json')
+  assert DataLoader._load_file(test_file)
+
+def test_load_ticket_file():
+  current_dir = os.path.dirname(os.path.realpath(__file__))
+  test_file = os.path.join(current_dir, 'resources', 'test_tickets.json')
+  assert DataLoader._load_file(test_file)
